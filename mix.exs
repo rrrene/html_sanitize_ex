f5b1fca16def1b3dfb0f8c70af82b7e1e8d03f53
René Föhring <rf@bamaru.de>
2015-07-05 19:09:53 +0200

    Bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HtmlSanitizeEx.Mixfile do
 
   def project do
     [app: :html_sanitize_ex,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
