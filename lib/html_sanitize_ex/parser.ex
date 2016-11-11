5b56a844f9166e0c9e62730b47f71cf23645148c
René Föhring <rf@bamaru.de>
2016-11-11 15:52:53 +0100

    Replace newlines and other white-space before parsing

--- lib/html_sanitize_ex/parser.ex
+++ lib/html_sanitize_ex/parser.ex
@@ -10,10 +10,21 @@ defmodule HtmlSanitizeEx.Parser do  @doc """
   @type html_tree :: tuple | list
 
   @my_root_node "html_sanitize_ex"
+  @text_replacements %{
+    "\n" => to_string([32, 255, 254, 43, 253, 32]),
+    "\r" => to_string([32, 255, 254, 47, 253, 32]),
+    "\t" => to_string([32, 255, 254, 56, 253, 32]),
+  }
 
   @spec parse(binary) :: html_tree
 
   def parse(html) do
+    html =
+      @text_replacements
+      |> Enum.reduce(html, fn({text, replacement}, memo) ->
+          memo |> String.replace(text, replacement)
+        end)
+
     html = "<#{@my_root_node}>#{html}</#{@my_root_node}>"
     {@my_root_node, [], parsed} = :mochiweb_html.parse(html)
 
@@ -21,12 +32,18 @@ defmodule HtmlSanitizeEx.Parser do  @doc """
   end
 
   def to_html(tokens) do
-    {@my_root_node, [], ensure_list(tokens)}
-    |> :mochiweb_html.to_html
-    |> Enum.join
-    |> String.replace(~r/^<#{@my_root_node}>/, "")
-    |> String.replace(~r/<\/#{@my_root_node}>$/, "")
-    |> String.replace("&lt;/html_sanitize_ex&gt;", "")
+    html =
+      {@my_root_node, [], ensure_list(tokens)}
+      |> :mochiweb_html.to_html
+      |> Enum.join
+      |> String.replace(~r/^<#{@my_root_node}>/, "")
+      |> String.replace(~r/<\/#{@my_root_node}>$/, "")
+      |> String.replace("&lt;/#{@my_root_node}&gt;", "")
+
+    @text_replacements
+    |> Enum.reduce(html, fn({text, replacement}, memo) ->
+        memo |> String.replace(replacement, text)
+      end)
   end
 
   defp ensure_list(list) do
