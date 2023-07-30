4de9ce3845693c90e77f9e99cc16a0f194213082
René Föhring <rf@bamaru.de>
2023-07-30 21:52:00 +0200

    Bump version to 1.4.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HtmlSanitizeEx.Mixfile do
   def project do
     [
       app: :html_sanitize_ex,
-      version: "1.4.2",
+      version: "1.4.3",
       elixir: "~> 1.0",
       description: "HTML sanitizer for Elixir",
       source_url: "https://github.com/rrrene/html_sanitize_ex",
